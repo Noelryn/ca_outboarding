eb7c33920a25d8f4f26a8456a7b7880cd17e5f61
Noel Ryan <noelgerard.ryan@gmail.com>
2019-02-20 20:27:44 +0100

    Adding the foreign key in to the create letter migrate file, updating the schema

--- db/migrate/20190206121003_create_letters.rb
+++ db/migrate/20190206121003_create_letters.rb
@@ -4,6 +4,8 @@ class CreateLetters < ActiveRecord::Migration[5.2]
       t.string :title
       t.text :content
 
+      t.references :user, foreign_key: true
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,10 @@ ActiveRecord::Schema.define(version: 2019_02_06_123439) do
   create_table "letters", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_letters_on_user_id"
   end
 
   create_table "resources", force: :cascade do |t|
@@ -84,6 +86,7 @@ ActiveRecord::Schema.define(version: 2019_02_06_123439) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "letters", "users"
   add_foreign_key "resources", "users"
   add_foreign_key "skills", "users"
 end
