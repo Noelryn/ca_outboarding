e93f774d1c38088d3d3c5841e1227de2d47fdd76
Noel Ryan <noelgerard.ryan@gmail.com>
2019-02-20 20:40:19 +0100

    permit user id in letters controller, adding the flash notice A new letter of recomendation has been created

--- app/controllers/admin/letters_controller.rb
+++ app/controllers/admin/letters_controller.rb
@@ -9,10 +9,11 @@ class Admin::LettersController < ApplicationController
 
   def create
     @member = User.find(params[:user_id])
-    @member.letter.create(params.require(:letter).permit(:title, :content))
+    @member.letter.create(params.require(:letter).permit(:title, :content, :user_id))
 
     if @letter.save
       redirect_to admin_letters_path
+      flash[:notice] = "A new letter of recomendation has been created"
     else
       render 'new'
     end
